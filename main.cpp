37befed6bb38a007ab55f1caf3c6f9cfe3e7e3a4
pt207 <47851246+Hay333@users.noreply.github.com>
2019-07-12 09:52:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
+
 using namespace std;
+
+void calcMiddle(const string file);
 
 int main(int argc, char **argv)
 {
-	int chislo[10], i;
-    for (i = 0; i < 10; i++)
-    {
-        cin >> chislo[i];
-    }
-    for (i = 0; i < 10; i++)
-    {
-        cout << setw(7) << oct << chislo[i] << setw(7);
-        cout << setw(7) << dec << chislo[i] << setw(7);
-        cout << setw(7) << hex << chislo[i] << endl;
-    }
+
+
+	char * file = "/home/vladimir/Загрузки/data_v7.txt";
+
+	calcMiddle((string)file);
 	return 0;
+}
+
+void calcMiddle(const string file)
+{
+
+	const int SIZE = 5;
+	float array[SIZE];
+	ifstream ifs;
+	ifs.open(file, ios::binary | ios::in);
+
+	ifs.seekg (0, std::ios::end);
+	int size = ifs.tellg();
+	ifs.seekg(0,ios::beg);
+
+	ifs.read((char*)array, 20);z
+
+	ifs.close();
+
+	float summ = 0;
+	for (int i = 0; i < SIZE; i++) {
+		cout << array[i] << endl;
+		summ +=array[i];
+	}
+	cout << "СРДЕНЕЕ АРИФМЕТИЧЕСКОЕ:"<< summ/SIZE << endl;
 }
