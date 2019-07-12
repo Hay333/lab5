736a33692a481ad0d2cbabe41fa77cbc4a52e36e
pt207 <47851246+Hay333@users.noreply.github.com>
2019-07-12 09:50:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
+
 using namespace std;
+
+void printTabl(const int mode);
 
 int main(int argc, char **argv)
 {
-	int chislo[10], i;
-    for (i = 0; i < 10; i++)
-    {
-        cin >> chislo[i];
-    }
-    for (i = 0; i < 10; i++)
-    {
-        cout << setw(7) << oct << chislo[i] << setw(7);
-        cout << setw(7) << dec << chislo[i] << setw(7);
-        cout << setw(7) << hex << chislo[i] << endl;
-    }
+	printTabl(5);
+	printTabl(8);
+	printTabl(10);
+	printTabl(16);
+
 	return 0;
+}
+
+void printTabl(const int mode)
+{
+
+	if (mode == 8) {
+		cout << "Режим "<< dec << mode << endl;
+		for (int i = 0; i < mode; i++) {
+			for (int j = 0; j < mode; j++) {
+				cout << oct << setw(4) << (i+1)*(j+1);
+			}
+			cout << endl;
+		}
+		return;
+	}
+	if (mode == 10) {
+		cout << "Режим " << dec << mode << endl;
+		for (int i = 0; i < mode; i++) {
+			for (int j = 0; j < mode; j++) {
+				cout << dec << setw(4) << (i+1)*(j+1);
+			}
+			cout << endl;
+		}
+		return;
+	}
+	if (mode == 16) {
+		cout << "Режим " <<dec << mode << endl;
+		for (int i = 0; i < mode; i++) {
+			for (int j = 0; j < mode; j++) {
+				cout << hex << setw(4) << (i+1)*(j+1);
+			}
+			cout << endl;
+		}
+		return;
+	}
+	cout << "unknown mode\n";
+	return;
 }
